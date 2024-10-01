130250d2df90e2c81c3eebe12d78de7a8dd4fbb6
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-01 22:26:04 +0200

    create Task entity

---
+++ backend/src/entities/Task.ts
@@ -0,0 +1,31 @@
+import {
+	BaseEntity,
+	Column,
+	Entity,
+	PrimaryGeneratedColumn,
+	ManyToMany,
+	JoinTable,
+} from "typeorm";
+import { Tag } from "./Tag";
+
+@Entity()
+export class Task extends BaseEntity {
+	@PrimaryGeneratedColumn()
+	id!: number;
+
+	@Column({ length: 200 })
+	title!: string;
+
+	@Column()
+	description?: string;
+
+	@Column()
+	createdAt!: Date;
+
+	@Column()
+	isComplete!: boolean;
+
+	@ManyToMany(() => Tag)
+	@JoinTable()
+	tags!: Tag[];
+}
