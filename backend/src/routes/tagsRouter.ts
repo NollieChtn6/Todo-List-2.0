5541de8af3118c619db31546dcd3c411c0869fe5
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-01 23:01:58 +0200

    create first routes for tags and tasks

---
+++ backend/src/routes/tagsRouter.ts
@@ -0,0 +1,7 @@
+export const tagsRouter = require("express").Router();
+
+tagsRouter.get("/tags");
+// tagsRouter.get("/tags/tag:id");
+// tagsRouter.post("/tags/create");
+// tagsRouter.put("/tags/tag:id/edit");
+// tagsRouter.delete("/tags/tasg:id/delete");
---
+++ backend/src/routes/tasksRouter.ts
@@ -0,0 +1,7 @@
+export const tasksRouter = require("express").Router();
+
+tasksRouter.get("/tasks");
+// tasksRouter.get("/tasks/task:id");
+// tasksRouter.post("/tasks/create");
+// tasksRouter.put("/tasks/task:id/edit");
+// tasksRouter.delete("/tasks/task:id/delete");
