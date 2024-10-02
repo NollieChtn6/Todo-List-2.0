fce4c150abe91cfe889a7ec248d869e05291873c
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-02 22:48:28 +0200

    import errorHandler

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,8 @@
 import Express from "express";
 import "reflect-metadata";
 import { router } from "./routes/router";
-
 import { dataSource } from "./config/db";
+import { errorHandler } from "./middlewares/errorHandler";
 
 const app = Express();
 const PORT = 3000;
@@ -10,6 +10,7 @@ const PORT = 3000;
 app.use(Express.json());
 
 app.use("/api", router);
+app.use(errorHandler);
 
 app.listen(PORT, async () => {
 	await dataSource.initialize();
