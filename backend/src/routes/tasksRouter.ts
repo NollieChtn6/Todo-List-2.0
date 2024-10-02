3b2c17ed780e7ae477c8aab64b6cac9c737471cf
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-02 22:58:25 +0200

    import getAllTasks and getTaskById methods from tasksController on routes '/tasks' and '/tasks/:id'

--- backend/src/routes/tasksRouter.ts
+++ backend/src/routes/tasksRouter.ts
@@ -1,7 +1,9 @@
 export const tasksRouter = require("express").Router();
 
-tasksRouter.get("/tasks");
-// tasksRouter.get("/tasks/task:id");
+import * as tasksController from "../controllers/tasksController";
+
+tasksRouter.get("/tasks", tasksController.getAllTasks);
+tasksRouter.get("/tasks/:id", tasksController.getTaskById);
 // tasksRouter.post("/tasks/create");
 // tasksRouter.put("/tasks/task:id/edit");
 // tasksRouter.delete("/tasks/task:id/delete");
