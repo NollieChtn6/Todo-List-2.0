dce44080c9979f28b72e90a676a08b2e0b35dedb
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-02 23:21:34 +0200

    import getAllTags and getTagById methods from tagsController on routes '/tags' and '/tags/:id'

--- backend/src/routes/tagsRouter.ts
+++ backend/src/routes/tagsRouter.ts
@@ -1,7 +1,10 @@
 export const tagsRouter = require("express").Router();
 
-tagsRouter.get("/tags");
-// tagsRouter.get("/tags/tag:id");
+import * as tagsController from "../controllers/tagsController";
+
+tagsRouter.get("/tags", tagsController.getAllTags);
+tagsRouter.get("/tags/:id", tagsController.getTagById);
+// tagRouter.get("/tags/id:/tasks")
 // tagsRouter.post("/tags/create");
 // tagsRouter.put("/tags/tag:id/edit");
 // tagsRouter.delete("/tags/tasg:id/delete");
