e6cea4b8dcff003126279756024e8eb60cbbbdc2
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-03 13:54:38 +0200

    create service handling task creation

--- backend/src/services/tasksServices.ts
+++ backend/src/services/tasksServices.ts
@@ -2,7 +2,7 @@ import { Tag } from "../entities/Tag";
 import { Task } from "../entities/Task";
 import { In } from "typeorm";
 
-export const getAllTasks = async () => {
+export const getAllTasks = async (): Promise<Task[]> => {
 	const tasks = await Task.find({
 		relations: ["tags"],
 	});
@@ -17,63 +17,24 @@ export const getTaskById = async (taskId: number) => {
 	return task;
 };
 
-// export const createNewTask = async (data: {
-// 	title: string;
-// 	description?: string;
-// 	tags: { id: number, label: string, color: string }[];
-// }) => {
-// 	const newTask = new Task();
-// 	newTask.title = data.title;
-// 	newTask.description = data.description;
-// 	newTask.isComplete = false;
-// 	newTask.createdAt = new Date();
+export const createNewTask = async (data: {
+	title: string;
+	description?: string;
+	tags: { id: number }[];
+}) => {
+	const newTask = new Task();
+	newTask.title = data.title;
+	newTask.description = data.description;
+	newTask.isComplete = false;
+	newTask.createdAt = new Date();
 
-// 	const getTagsId = await Tag.find()
+	if (data.tags && data.tags.length > 0) {
+		const tagIds = data.tags.map((tag) => tag.id);
+		console.log("Tags Ids:", tagIds);
+		const tags = await Tag.findBy({ id: In(tagIds) });
+		console.log("Tags:", tags);
+		newTask.tags = tags;
+	}
 
-// 	const tagsInTask = await Tag.findOneBy({id: tagId})
-
-// 	// if (data.tags && data.tags.length > 0) {
-// 	// 	const tagIds = data.tags.map((tag) => tag.id);
-// 	// 	const existingTags = await Tag.find({
-// 	// 		where: { id: In(tagIds) },
-// 	// 	});
-// 	// 	console.log("Existing tags:", existingTags);
-// 	// 	newTask.tags = existingTags;
-// 	// }
-
-// 	return await Task.save(newTask);
-// };
-
-// app.post("/ads/create", async (req: Express.Request, res: Express.Response) => {
-// 	const {
-// 		title,
-// 		description,
-// 		owner,
-// 		price,
-// 		picture,
-// 		location,
-// 		createdAt,
-// 		categoryId,
-// 	} = req.body;
-// 	console.log("Req body:", req.body);
-// 	try {
-// 		const newAd = new Ad();
-// 		newAd.title = title;
-// 		newAd.description = description;
-// 		newAd.owner = owner;
-// 		newAd.price = price;
-// 		newAd.picture = picture;
-// 		newAd.location = location;
-// 		newAd.createdAt = createdAt;
-
-// 		const category = await Category.findOneBy({ id: categoryId });
-// 		if (category) {
-// 			newAd.category = category;
-// 		}
-// 		newAd.save();
-// 		res.status(201).send(newAd);
-// 	} catch (err) {
-// 		console.log(err);
-// 		res.status(500).send(err);
-// 	}
-// });
+	return await Task.save(newTask);
+};
