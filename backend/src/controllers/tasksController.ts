f25e0bcdcb06df24aadb4e88e6ebc3a94feab679
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-03 19:24:52 +0200

    format file

--- backend/src/controllers/tasksController.ts
+++ backend/src/controllers/tasksController.ts
@@ -1,100 +1,80 @@
 import type { Request, Response, NextFunction } from "express";
 import * as tasksServices from "../services/tasksServices";
 
-export const getAllTasks = async (
-	req: Request,
-	res: Response,
-	next: NextFunction,
-) => {
-	try {
-		const tasks = await tasksServices.getAllTasks();
-		res.json(tasks);
-	} catch (err) {
-		next(err);
-	}
+export const getAllTasks = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const tasks = await tasksServices.getAllTasks();
+    res.json(tasks);
+  } catch (err) {
+    next(err);
+  }
 };
 
-export const getTaskById = async (
-	req: Request,
-	res: Response,
-	next: NextFunction,
-) => {
-	try {
-		const taskId = Number(req.params.id);
-		const task = await tasksServices.getTaskById(taskId);
-		if (!task) {
-			return res.status(404).json({ message: "Task not found" });
-		}
-		res.json(task);
-	} catch (err) {
-		next(err);
-	}
+export const getTaskById = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const taskId = Number(req.params.id);
+    const task = await tasksServices.getTaskById(taskId);
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+    res.json(task);
+  } catch (err) {
+    next(err);
+  }
 };
 
-export const createNewTask = async (
-	req: Request,
-	res: Response,
-	next: NextFunction,
-) => {
-	try {
-		const { title, description, tags } = req.body;
-		const newTask = await tasksServices.createNewTask({
-			title,
-			description,
-			tags: tags || [],
-		});
-		const responseTask = {
-			id: newTask.id,
-			title: newTask.title,
-			description: newTask.description,
-			tags: newTask.tags,
-		};
+export const createNewTask = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { title, description, tags } = req.body;
+    const newTask = await tasksServices.createNewTask({
+      title,
+      description,
+      tags: tags || [],
+    });
+    const createdTask = {
+      id: newTask.id,
+      title: newTask.title,
+      description: newTask.description,
+      tags: newTask.tags,
+    };
 
-		res.status(201).json(responseTask);
-	} catch (err) {
-		next(err);
-	}
+    res.status(201).json(createdTask);
+  } catch (err) {
+    next(err);
+  }
 };
 
-export const deleteTag = async (
-	req: Request,
-	res: Response,
-	next: NextFunction,
-) => {
-	try {
-		const taskId = Number(req.params.id);
-		const task = await tasksServices.deleteTask(taskId);
-		if (!task) {
-			return res.status(404).json({ message: "Task not found" });
-		}
-		// res.json(task);
-		res.status(204).send("Task has successfully been deleted");
-	} catch (err) {
-		next(err);
-	}
+export const deleteTag = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const taskId = Number(req.params.id);
+    const task = await tasksServices.deleteTask(taskId);
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+    // res.json(task);
+    res.status(204).send("Task has successfully been deleted");
+  } catch (err) {
+    next(err);
+  }
 };
 
-export const updateTask = async (
-	req: Request,
-	res: Response,
-	next: NextFunction,
-) => {
-	try {
-		const taskId = Number(req.params.id);
-		const { title, description, tags } = req.body;
+export const updateTask = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const taskId = Number(req.params.id);
+    const { title, description, tags } = req.body;
 
-		const updatedTask = await tasksServices.updateTask(taskId, {
-			title,
-			description,
-			tags,
-		});
+    const updatedTask = await tasksServices.updateTask(taskId, {
+      title,
+      description,
+      tags,
+    });
 
-		if (!updatedTask) {
-			return res.status(404).json({ message: "Task not found" });
-		}
+    if (!updatedTask) {
+      return res.status(404).json({ message: "Task not found" });
+    }
 
-		res.status(204).json(updatedTask);
-	} catch (err) {
-		next(err);
-	}
+    res.status(204).json(updatedTask);
+  } catch (err) {
+    next(err);
+  }
 };
