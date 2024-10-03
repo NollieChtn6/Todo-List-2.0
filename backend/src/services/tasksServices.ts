6d17e1c40a36badda1389e3351177a0b4bfc80fb
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-03 14:05:01 +0200

    specify promises type

--- backend/src/services/tasksServices.ts
+++ backend/src/services/tasksServices.ts
@@ -9,7 +9,7 @@ export const getAllTasks = async (): Promise<Task[]> => {
 	return tasks;
 };
 
-export const getTaskById = async (taskId: number) => {
+export const getTaskById = async (taskId: number): Promise<Task | null> => {
 	const task = await Task.findOne({
 		where: { id: taskId },
 		relations: ["tags"],
@@ -21,7 +21,7 @@ export const createNewTask = async (data: {
 	title: string;
 	description?: string;
 	tags: { id: number }[];
-}) => {
+}): Promise<Task> => {
 	const newTask = new Task();
 	newTask.title = data.title;
 	newTask.description = data.description;
