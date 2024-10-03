1bf9b6860503ef7ce0e1aad91a5d98b46fb6ce56
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-03 12:30:26 +0200

    create route '/tags/create' and use method createNewTag from tagsController

--- backend/src/routes/tagsRouter.ts
+++ backend/src/routes/tagsRouter.ts
@@ -5,6 +5,6 @@ import * as tagsController from "../controllers/tagsController";
 tagsRouter.get("/tags", tagsController.getAllTags);
 tagsRouter.get("/tags/:id", tagsController.getTagById);
 // tagRouter.get("/tags/id:/tasks")
-// tagsRouter.post("/tags/create");
+tagsRouter.post("/tags/create", tagsController.createNewTag);
 // tagsRouter.put("/tags/tag:id/edit");
 // tagsRouter.delete("/tags/tasg:id/delete");
