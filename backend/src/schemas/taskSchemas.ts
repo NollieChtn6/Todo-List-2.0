f680ed45e196d4521702fbd88625ad01b637a6c7
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-03 23:52:43 +0200

    create input validation schemas on create and update task and infer new type accordingly

---
+++ backend/src/schemas/taskSchemas.ts
@@ -0,0 +1,27 @@
+import { z } from "zod";
+
+export const createTaskSchema = z.object({
+  title: z
+    .string({ message: "title must be a string" })
+    .min(4, { message: "title must have at least 4 characters" })
+    .max(200),
+  description: z.string({ message: "description must be a string" }).optional(),
+  tags: z
+    .array(z.object({ id: z.number().int() }), { message: "tags must be an array of objects" })
+    .optional(),
+});
+
+export type CreateTaskAPIInput = z.infer<typeof createTaskSchema>;
+
+export const updateTaskSchema = z.object({
+  title: z
+    .string({ message: "title must be a string" })
+    .min(4, { message: "title must have at least 4 characters" })
+    .max(200),
+  description: z.string({ message: "description must be a string" }).optional(),
+  tags: z
+    .array(z.object({ id: z.number().int() }), { message: "tags must be an array of objects" })
+    .optional(),
+});
+
+export type UpdateTaskAPIInput = z.infer<typeof updateTaskSchema>;
