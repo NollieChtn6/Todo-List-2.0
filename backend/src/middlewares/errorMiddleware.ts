94b96dda307969cdc0c15e6e44121933b305f149
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-03 22:51:49 +0200

    create new error handler middleware

---
+++ backend/src/middlewares/errorMiddleware.ts
@@ -0,0 +1,22 @@
+// Code snippets from:
+// https://dev.to/osalumense/validating-request-data-in-expressjs-using-zod-a-comprehensive-guide-3a0j
+// https://jeffsegovia.dev/blogs/rest-api-validation-using-zod
+
+import type { NextFunction, Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import { ZodError, type z } from "zod";
+
+export async function catchValidationErrors(_req: Request, res: Response, next: NextFunction) {
+  try {
+    next();
+  } catch (error) {
+    if (error instanceof ZodError) {
+      const errorMessages = error.errors.map((issue: z.ZodIssue) => ({
+        message: `On '${issue.path.join(".")}': ${issue.message}`,
+      }));
+      res.status(StatusCodes.BAD_REQUEST).json({ error: "Invalid data", details: errorMessages });
+    } else {
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: "Internal Server Error" });
+    }
+  }
+}
