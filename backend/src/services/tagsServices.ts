61194fba1c73d7d201a33e959fc3b75c59cbd7f7
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-03 14:03:56 +0200

    specify promises type

--- backend/src/services/tagsServices.ts
+++ backend/src/services/tagsServices.ts
@@ -1,18 +1,21 @@
 import { Tag } from "../entities/Tag";
 
-export const getAllTags = async () => {
+export const getAllTags = async (): Promise<Tag[]> => {
 	const tags = await Tag.find();
 	return tags;
 };
 
-export const getTagById = async (tagId: number) => {
+export const getTagById = async (tagId: number): Promise<Tag | null> => {
 	const tag = await Tag.findOne({
 		where: { id: tagId },
 	});
 	return tag;
 };
 
-export const createNewTag = async (label: string, color: string) => {
+export const createNewTag = async (
+	label: string,
+	color: string,
+): Promise<Tag> => {
 	const tag = await Tag.findOne({ where: { label } });
 	if (!tag) {
 		const newTag = new Tag();
