989b6e98b3c54d2463598e27241ccccfd9dafda6
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-03 16:22:49 +0200

    create '/tasks/:id/update' route with PATCH method and use updateTask method from tasksController

--- backend/src/routes/tasksRouter.ts
+++ backend/src/routes/tasksRouter.ts
@@ -6,5 +6,5 @@ tasksRouter.get("/tasks", tasksController.getAllTasks);
 tasksRouter.get("/tasks/:id", tasksController.getTaskById);
 // taskRouter.get("/tasks/:id/tags")
 tasksRouter.post("/tasks/create", tasksController.createNewTask);
-// tasksRouter.put("/tasks/task:id/edit");
+tasksRouter.patch("/tasks/:id/update", tasksController.updateTask);
 tasksRouter.delete("/tasks/:id/delete", tasksController.deleteTag);
