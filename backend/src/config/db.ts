d67aa36ff7bca7af719744adbda1bbee2f530cb6
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-03 23:41:23 +0200

    fix: update name of entities

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,11 +1,11 @@
 import { DataSource } from "typeorm";
 
-import { Tag } from "../entities/Tag";
-import { Task } from "../entities/Task";
+import { TagSQLiteModel } from "../entities/Tag";
+import { TaskSQLiteModel } from "../entities/Task";
 
 export const dataSource = new DataSource({
-	type: "sqlite",
-	database: "./to-do-list.sqlite",
-	entities: [Task, Tag],
-	synchronize: true,
+  type: "sqlite",
+  database: "./to-do-list.sqlite",
+  entities: [TaskSQLiteModel, TagSQLiteModel],
+  synchronize: true,
 });
