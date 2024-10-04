c5e0c427da165acf28d80f923d75bd6821845883
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-04 14:51:43 +0200

    import router and check component library and routes with basic setting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,20 @@
-import "./App.css";
+import { Router } from "./Router";
+import { match } from "ts-pattern";
+import { Homepage } from "./pages/HomePage";
+import { Button } from "primereact/button";
 
 function App() {
-  return (
-    <>
-      <p>To do list is coming!</p>
-    </>
-  );
+	const route = Router.useRoute(["Home"]);
+	return (
+		<>
+			{match(route)
+				.with({ name: "Home" }, () => <Homepage />)
+				.otherwise(() => (
+					<h1 className="m-auto">Oops! Not found!</h1>
+				))}
+			<Button>This is a test</Button>
+		</>
+	);
 }
 
 export default App;
