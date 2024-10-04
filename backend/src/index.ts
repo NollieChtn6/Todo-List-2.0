bd66098b1dafaf96ca81a991149b6b0c1a4aba73
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-04 15:06:45 +0200

    import cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,15 +7,17 @@ import "reflect-metadata";
 import { router } from "./routes/router";
 import { dataSource } from "./config/db";
 import { catchValidationErrors } from "./middlewares/errorMiddleware";
+import cors from "cors";
 
 const app = Express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(Express.json());
 app.use(catchValidationErrors);
 app.use("/api", router);
 
 app.listen(PORT, async () => {
-  await dataSource.initialize();
-  console.log(`App listening on: http://localhost:${PORT}`);
+	await dataSource.initialize();
+	console.log(`App listening on: http://localhost:${PORT}`);
 });
