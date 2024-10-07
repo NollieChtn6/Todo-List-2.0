832188bd7b1d9bb0afe96e22c0c4e92eb8487fd8
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-07 07:26:49 +0200

    create tags store

---
+++ frontend/src/store/tagStore.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { create } from "zustand";
+import type { Tag } from "../@types/types";
+
+type TagsStore = {
+	tags: Tag[];
+	fetchTags: () => Promise<void>;
+};
+
+export const useTagsStore = create<TagsStore>((set) => ({
+	tags: [],
+	fetchTags: async () => {
+		try {
+			const response = await axios.get("http://localhost:3000/api/tags");
+			if (response.data) {
+				set({ tags: response.data });
+				console.log(response.data);
+			} else {
+				throw new Error("Failed to fetch tasgs");
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	},
+}));
