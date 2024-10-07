2440646f66a2cf0b476d661df7ca6289bea2de34
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-07 07:25:58 +0200

    import stylesheets from prime react

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { PrimeReactProvider } from "primereact/api";
-import "primereact/resources/themes/mdc-dark-deeppurple/theme.css";
+// import "primereact/resources/themes/mdc-dark-deeppurple/theme.css";
+import "primereact/resources/themes/soho-light/theme.css";
+import "primeicons/primeicons.css";
 import "./index.css";
 
 // https://www.codingdeft.com/posts/react-18-typescript-error/
