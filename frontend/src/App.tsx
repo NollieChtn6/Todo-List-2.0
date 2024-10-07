1338a9cb589e73ed5fa9e2983d627761e3f0b242
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-07 07:27:14 +0200

    import data from tasks and tags stores

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,33 @@
+import { useEffect } from "react";
+
 import { Router } from "./Router";
 import { match } from "ts-pattern";
 import { Homepage } from "./pages/HomePage";
-import { Button } from "primereact/button";
+
+import { useTasksStore } from "./store/taskStore";
+import { useTagsStore } from "./store/tagStore";
 
 function App() {
 	const route = Router.useRoute(["Home"]);
+	const fetchTasks = useTasksStore((state) => state.fetchTasks);
+	const fetchTags = useTagsStore((state) => state.fetchTags);
+
+	useEffect(() => {
+		const loadData = async () => {
+			await fetchTasks();
+			await fetchTags();
+		};
+		loadData();
+	}, [fetchTasks, fetchTags]);
+
 	return (
 		<>
+			{/* <h1>My todo list</h1> */}
 			{match(route)
 				.with({ name: "Home" }, () => <Homepage />)
 				.otherwise(() => (
 					<h1 className="m-auto">Oops! Not found!</h1>
 				))}
-			<Button>This is a test</Button>
 		</>
 	);
 }
