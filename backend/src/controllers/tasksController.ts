8ee364c113f01a5ed36bfa0f494ed9e49cc0bdfd
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-07 07:30:49 +0200

    use updateTaskStatus service to update task in database

--- backend/src/controllers/tasksController.ts
+++ backend/src/controllers/tasksController.ts
@@ -4,44 +4,70 @@ import * as tasksServices from "../services/tasksServices";
 import * as tasksSchemas from "../schemas/taskSchemas";
 
 export const getAllTasks = async (req: Request, res: Response) => {
-  const tasks = await tasksServices.getAllTasks();
-  res.json(tasks);
+	const tasks = await tasksServices.getAllTasks();
+	res.json(tasks);
 };
 
 export const getTaskById = async (req: Request, res: Response) => {
-  const taskId = Number(req.params.id);
-  const task = await tasksServices.getTaskById(taskId);
-  if (!task) {
-    return res.status(404).json({ message: "Task not found" });
-  }
-  res.json(task);
-  return task;
+	const taskId = Number(req.params.id);
+	const task = await tasksServices.getTaskById(taskId);
+	if (!task) {
+		return res.status(404).json({ message: "Task not found" });
+	}
+	res.json(task);
+	return task;
 };
 
 export const createNewTask = async (req: Request, res: Response) => {
-  const { title, description, tags } = tasksSchemas.updateTaskSchema.parse(req.body);
-  const newTask = await tasksServices.createNewTask({ title, description, tags });
-  res.status(201).json({ message: "Task has successfully been created:", newTask });
+	const { title, description, tags } = tasksSchemas.updateTaskSchema.parse(
+		req.body,
+	);
+	const newTask = await tasksServices.createNewTask({
+		title,
+		description,
+		tags,
+	});
+	res
+		.status(201)
+		.json({ message: "Task has successfully been created:", newTask });
 };
 
 export const deleteTag = async (req: Request, res: Response) => {
-  const taskId = Number(req.params.id);
-  const task = await tasksServices.deleteTask(taskId);
-  if (!task) {
-    return res.status(404).json({ message: "Task not found" });
-  }
-  res.status(200).send("Task has successfully been deleted");
+	const taskId = Number(req.params.id);
+	const task = await tasksServices.deleteTask(taskId);
+	if (!task) {
+		return res.status(404).json({ message: "Task not found" });
+	}
+	res.status(200).send("Task has successfully been deleted");
 };
 
 export const updateTask = async (req: Request, res: Response) => {
-  const taskId = Number(req.params.id);
-  const { title, description, tags } = tasksSchemas.updateTaskSchema.parse(req.body);
+	const taskId = Number(req.params.id);
+	const { title, description, tags } = tasksSchemas.updateTaskSchema.parse(
+		req.body,
+	);
 
-  const updatedTask = await tasksServices.updateTask(taskId, {
-    title,
-    description,
-    tags,
-  });
+	const updatedTask = await tasksServices.updateTask(taskId, {
+		title,
+		description,
+		tags,
+	});
 
-  res.status(200).json({ message: "Task has successfully been updated", updatedTask });
+	res
+		.status(200)
+		.json({ message: "Task has successfully been updated", updatedTask });
+};
+
+export const updateTaskStatus = async (req: Request, res: Response) => {
+	const taskId = Number(req.params.id);
+	const { isComplete } = tasksSchemas.updateTaskStatusSchema.parse(req.body);
+
+	const updatedTaskStatus = await tasksServices.updateTaskStatus(taskId, {
+		isComplete,
+	});
+
+	res.status(200).json({
+		message: "Task status has successfully been updated",
+		updatedTaskStatus,
+	});
 };
