192ba5689235054d9fdbbbc0f43675b5ecc2e911
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-07 07:31:27 +0200

    create route '/tasks/:id/toggle-status' with PATCH method

--- backend/src/routes/tasksRouter.ts
+++ backend/src/routes/tasksRouter.ts
@@ -1,10 +1,12 @@
 export const tasksRouter = require("express").Router();
 
 import * as tasksController from "../controllers/tasksController";
+import { tagsRouter } from "./tagsRouter";
 
 tasksRouter.get("/tasks", tasksController.getAllTasks);
 tasksRouter.get("/tasks/:id", tasksController.getTaskById);
 // taskRouter.get("/tasks/:id/tags")
 tasksRouter.post("/tasks/create", tasksController.createNewTask);
 tasksRouter.patch("/tasks/:id/update", tasksController.updateTask);
+tagsRouter.patch("/tasks/:id/toggle-status", tasksController.updateTaskStatus);
 tasksRouter.delete("/tasks/:id/delete", tasksController.deleteTag);
